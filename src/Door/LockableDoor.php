e7d784ff0c993d04c8554f17a0c6bd5bf9a4ae20
Thomas Weldon <tomd90@gmail.com>
2019-06-16 14:48:19 +0100

    Corrected small errors

--- src/Door/LockableDoor.php
+++ src/Door/LockableDoor.php
@@ -2,11 +2,13 @@
 
 namespace Src\Door;
 
+use Src\Key;
+
 abstract class LockableDoor extends AbstractDoor
 {
-    abstract function lock();
+    abstract function lock(Key $key);
 
-    abstract function unlock();
+    abstract function unlock(Key $key);
 
     abstract function isLocked(): bool;
 }
